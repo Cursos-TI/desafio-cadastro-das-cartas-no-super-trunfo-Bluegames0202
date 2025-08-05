6bcfe0f9f8e6e64ced07a15b6ddadfa6a5d7b74c
Bluegames0202 <davibenedito23@gmail.com>
2025-08-05 00:40:31 +0000

    add duas funçoes

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,10 +5,11 @@ int main(){
 char codigodacarta1[20], codigodacarta2[20];
 char nomedacidade1[50], nomedacidade2[50];
     int populaçao1,populaçao2,numerodepontosturisticos1,numerodepontosturisticos2;
-    float area1,area2,pib1,pib2;
-//informações da primeira carta
+    float area1,area2,pib1,pib2,pibper1,pibper2,ds1,ds2;
+
+    //informações da primeira carta
     printf("Digite o estado da carta1:\n");
-    scanf(" %c",&estado1);
+    scanf(" %s",&estado1);
 
     printf("Digite o código da carta1: \n");
     scanf("%s",&codigodacarta1);
@@ -27,9 +28,13 @@ char nomedacidade1[50], nomedacidade2[50];
 
     printf("Digite a quantidade de pontos turisticos da carta1: \n");
     scanf("%d",&numerodepontosturisticos1);
- //INFORMAÇÕES DA SEGUNDA CARTA
+    ds1 = populaçao1 / area1;
+    pibper1 = pib1 / populaçao1;
+
+ 
+    //INFORMAÇÕES DA SEGUNDA CARTA
  printf("Digite o estado da carta2:\n");
-    scanf(" %c",&estado2);
+    scanf("%s",&estado2);
 
     printf("Digite o código da carta2: \n");
     scanf("%s",&codigodacarta2);
@@ -48,15 +53,24 @@ char nomedacidade1[50], nomedacidade2[50];
 
     printf("Digite a quantidade de pontos turisticos da carta2: \n");
     scanf("%d",&numerodepontosturisticos2);
+   
+     ds2 = populaçao2 / area2;
+    pibper2 = pib2 / populaçao2;
+   
+   
     //RESULTADO DAS CARTAS
  printf("Carta 1 \n");
- printf("Estado: %c \n",estado1);
- printf("Código: %s \n",codigodacarta1);
- printf("Cidade: %s \n",nomedacidade1);
+ printf("Estado:%s \n",estado1);
+ printf("Código:%s \n",codigodacarta1);
+ printf("Cidade:%s \n",nomedacidade1);
  printf("População: %d \n",populaçao1);
  printf("Área: %.2f km \n",area1);
  printf("PIB:%.2f bilhões de reais \n",pib1);
  printf("Números de pontos turísticos: %d \n",numerodepontosturisticos1);
+printf("Densidade populacao:%f hdb/km \n",ds1);
+printf("Dpibper:%f hdb/km \n",pibper1);
+
+
 
  printf("Carta 2 \n");
  printf("Estado: %c \n",estado2);
@@ -66,7 +80,8 @@ char nomedacidade1[50], nomedacidade2[50];
  printf("Área: %.2f km \n",area2);
  printf("PIB:%.2f bilhões de reais \n",pib2);
  printf("Números de pontos turísticos: %d \n",numerodepontosturisticos2);
-
+printf("Densidade populacao:%.2f hdb/km \n",ds2);
+printf("pibper:%.2f hdb/km \n",pibper2);
 
 
 
